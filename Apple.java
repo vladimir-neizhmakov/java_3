d7670c97d81d6f4176e682f0dff1c65a221b343d
Vladimir Neizhmakov <vovka_x@mail.ru>
2020-05-14 04:08:32 +0300

    ДЗ 1

---
+++ Apple.java
@@ -0,0 +1,10 @@
+/*
+a. Есть классы Fruit -> Apple, Orange;(больше фруктов не надо)
+ */
+public class Apple extends Fruit {
+    private float weight = 1.0f;
+
+    public float getWeight() {
+        return weight;
+    }
+}
